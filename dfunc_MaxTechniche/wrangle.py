2c34fc3f83e756b6c7617d7dec5f3e3853848189
MaxTechniche <Max.Techniche@gmail.com>
2020-10-27 14:23:27 -0600

    OOP style!

--- dfunc_MaxTechniche/wrangle.py
+++ dfunc_MaxTechniche/wrangle.py
@@ -1,21 +1,41 @@
-"""Wrangle functions for pandas DataFrames"""
+"""Better pandas DataFrame"""
 
 import pandas as pd
 import numpy as np
 import random
 
-def numna(df):
-    """Return number of null values in dataframe"""
-    return df.isna().sum()
 
-def train_test_split(df, test_size=.2, random_state=None):
-    """Return a train/test split of a dataframe"""
-    df_size = len(df)
-    testing_size = max(1, int(df_size*test_size))
-    test = df.sample(testing_size, random_state=random_state)
-    train = df.drop(test.index)
-    return train, test
+class BetterDF(pd.DataFrame):
+    """Pandas DataFrame with a couple more simple functions."""
 
-def shuffle(df, random_state=None):
-    """Return a shuffled version of the dataframe"""
-    return df.sample(frac=1, random_state=random_state)
+    def numna(self):
+        """Return number of null values in dataframe"""
+        return self.isna().sum()
+
+    def train_test_split(self, test_size=.2, random_state=None):
+        """Return a train/test split of a dataframe"""
+        
+        df_size = len(self)
+        testing_size = max(1, int(df_size*test_size))
+        
+        self.test = BetterDF(self.sample(testing_size, random_state=random_state))
+        self.train = BetterDF(self.drop(self.test.index))
+
+        return self.train, self.test
+
+    def shuffle(self, random_state=None, inplace=False):
+        """Return a shuffled version of the dataframe"""
+        df = BetterDF(self.sample(frac=1, random_state=random_state))
+        if inplace:
+            self = df
+        return df
+
+if __name__ == "__main__":
+    df = BetterDF({
+        'A':range(20),
+        'B':range(20, 40)
+    })
+
+    df.train_test_split()
+    df.shuffle()
+    print(df.numna())
